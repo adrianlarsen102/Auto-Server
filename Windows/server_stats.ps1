73fd669ee0501e945144183405600507a3fd7395
Pizporaldo <alexander.stegarevic@gmail.com>
2025-05-16 09:55:15 +0200

    Update server_stats.ps1

--- Windows/server_stats.ps1
+++ Windows/server_stats.ps1
@@ -2,18 +2,21 @@ param (
     [string]$ServerName = "10.14.2.87"
 )
 
+# First, obtain your credentials interactively and store them in a variable.
+$credential = Get-Credential
+
 $remoteScript = {
     $results = @()
 
     $results += "=== SYSTEM STATUS FOR $($env:COMPUTERNAME) ===`n"
 
     # CPU Usage
-    $cpuLoad = (Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
+    $cpuLoad = (Get-CimInstance -ClassName Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
     $results += "-- CPU Usage --"
     $results += "$cpuLoad % used`n"
 
     # Memory Usage
-    $os = Get-CimInstance Win32_OperatingSystem
+    $os = Get-CimInstance -ClassName Win32_OperatingSystem
     $totalMem = [math]::Round($os.TotalVisibleMemorySize / 1024, 2)
     $freeMem = [math]::Round($os.FreePhysicalMemory / 1024, 2)
     $results += "-- Memory Usage --"
@@ -21,7 +24,7 @@ $remoteScript = {
 
     # Disk Usage
     $results += "-- Disk Usage --"
-    Get-CimInstance Win32_LogicalDisk -Filter "DriveType=3" | ForEach-Object {
+    Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" | ForEach-Object {
         $free = [math]::Round($_.FreeSpace / 1GB, 2)
         $size = [math]::Round($_.Size / 1GB, 2)
         $results += "$($_.DeviceID): $free GB free of $size GB"
@@ -29,11 +32,12 @@ $remoteScript = {
     $results += ""
 
     # Uptime
-    $boot = (Get-CimInstance Win32_OperatingSystem).LastBootUpTime
+    $boot = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
     $results += "-- Uptime --"
     $results += "Last boot time: $boot"
 
     $results -join "`n"
 }
 
-Invoke-Command -ComputerName $ServerName -Credential (Get-Credential) -ScriptBlock $remoteScript
+# Execute the remote script using the provided credentials.
+Invoke-Command -ComputerName $ServerName -Credential $credential -ScriptBlock $remoteScript
